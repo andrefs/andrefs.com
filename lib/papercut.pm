b7126b58979f2a53dc0b4e69867e485cfa067c47
André Santos <andrefs@cpan.org>
2012-06-17 02:33:10 +0100

    navmenu

--- lib/papercut.pm
+++ lib/papercut.pm
@@ -2,9 +2,15 @@ package papercut;
 use strict; use warnings;
 use Dancer ':syntax';
 load_app 'papercut::blog';
+use Data::Dump qw/pp dump/;
 #load_app 'papercut::deploy';
 
 our $VERSION = '0.1';
+
+hook after_file_render => sub {
+	session appname => config->{appname};
+	session menu 	=> config->{appmenu};
+};
 
 before_template sub {
     my $tokens = shift;
