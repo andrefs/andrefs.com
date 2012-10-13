9409f80836975a9b141fd32abd1bde0883ac36e6
André Santos <andrefs@cpan.org>
2012-10-13 18:31:57 +0100

    modified to run with starman

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/env perl
 use Dancer;
 use papercut;
-dance;
+
+use Plack::Builder;
+my $app = sub {
+    my $env = shift;
+
+    my $request = Dancer::Request->new( env => $env );
+    Dancer->dance($request);
+};
+
+builder {
+	enable_if { $_[0]->{REMOTE_ADDR} eq '127.0.0.1' } 
+		"Plack::Middleware::ReverseProxy";
+	enable_if { $ENV{STARMAN_DEBUG} }
+	 'Debug', panels => [qw/Parameters Dancer::Version Dancer::Settings Memory/];
+	$app;
+}
